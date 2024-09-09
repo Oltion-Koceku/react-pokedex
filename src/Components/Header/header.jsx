22b95b9500400cfe9e39c1e98e620f6efed25082
Oltion-Koceku <koceku17@gmail.com>
2024-09-09 15:47:32 +0200

    [scaffolding]

---
+++ src/Components/Header/header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <div className='Header'>
+      <div className="container">
+        <div className="menu">
+          <h1 className='title d-flex justify-content-center w-100'>Pokedex <span>Nazionale</span></h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default header
