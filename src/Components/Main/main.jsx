258a66b26d6a38a31f991d04e5ac6d795da5993e
Oltion-Koceku <koceku17@gmail.com>
2024-09-09 16:30:53 +0200

    [install] axios [all] Pokemon

--- src/Components/Main/main.jsx
+++ src/Components/Main/main.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import Axios from 'axios'
 
-const main = () => {
+
+const Main = () => {
+
+
+  const getApi = () =>{
+    Axios.get('https://pokeapi.co/api/v2/pokemon/')
+    .then(res =>{
+      console.log(res.data);
+    })
+    .catch(error => {
+      console.log(error.message);
+      
+    })
+  }
+
+  useEffect (() =>{
+    getApi();
+  })
+
+
   return (
-    <div>main</div>
-  )
-}
+    <div className="Main">
+      <div className="container">
+        <div className="searchMenu">
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2 w-25"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default main
+export default Main;
